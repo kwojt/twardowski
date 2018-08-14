d3e02c748b29d41de8c2c3533638199849b5b2a3
Krzysztof Wojt <kwojt@g.pl>
2018-08-14 12:21:48 +0200

    API Message change.

--- controllers/mainController.go
+++ controllers/mainController.go
@@ -6,9 +6,10 @@ import (
 	tc "github.com/kwojt/twardowski/commons"
 )
 
-const apiMessage = `Hello! This app uses following API:
-===================================
-/rand - GETs JSON with random poem`
+const apiMessage = `
+Hello! This app uses following API:<br>
+===================================<br>
+/rand - GETs JSON with random poem<br>`
 
 func DisplayAPI(w http.ResponseWriter, r *http.Request) {
 	tc.RespondHtml(w, apiMessage)
