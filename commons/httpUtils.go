d0af7b908d8a9a05e6191a9874749636d32d0a32
Krzysztof Wojt <kwojt@g.pl>
2018-08-14 15:38:55 +0200

    Added header to HTTP Response.

--- commons/httpUtils.go
+++ commons/httpUtils.go
@@ -5,16 +5,20 @@ import (
 	"net/http"
 )
 
+const allowControlAllowOrigin = "*"
+
 func Message(status bool, message string) map[string]interface{} {
 	return map[string]interface{}{"status": status, "message": message}
 }
 
 func Respond(w http.ResponseWriter, data map[string]interface{}) {
+	w.Header().Add("Access-Control-Allow-Origin", allowControlAllowOrigin)
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(data)
 }
 
 func RespondHtml(w http.ResponseWriter, body string) {
+	w.Header().Add("Access-Control-Allow-Origin", allowControlAllowOrigin)
 	w.Header().Add("Content-Type", "text/html")
 	w.Write([]byte(body))
 }
